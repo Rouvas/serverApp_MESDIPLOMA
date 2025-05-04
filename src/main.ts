fe20d36ef3fc01db9ed66d00ee6fa04163a79529
Rouvas <mail@gaiduchik.com>
2025-05-04 14:42:16 +0300

    minor change

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import * as morgan from 'morgan';
 import { AppModule } from './app.module';
-import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,19 +13,17 @@ async function bootstrap() {
   // 2) Логирование всех входящих запросов
   app.use(morgan('combined'));
 
-  // 3) Разрешить CORS (при необходимости указать настройки доменов)
+  // 3) Разрешить CORS
   app.enableCors();
 
   // 4) Глобальная валидация входящих DTO
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // отбрасывает неизвестные поля
-      forbidNonWhitelisted: true, // бросает ошибку при неизвестных полях
+      forbidNonWhitelisted: true, // возвращает ошибку при неизвестных полях
       transform: true, // автоматически преобразует payload в нужные типы
     }),
   );
-
-  app.use(passport.initialize());
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
