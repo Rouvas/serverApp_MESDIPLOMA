bf105d5fce2d655d7984109c2f07fe981722fa8e
Rouvas <mail@gaiduchik.com>
2025-05-22 17:35:36 +0300

    minor change

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,10 +1,10 @@
-import { IsString, IsEnum } from 'class-validator';
+import { IsString, IsEnum, IsOptional } from 'class-validator';
 import { Role, Sex } from '../schemas/user.schema';
 
 export class UpdateUserDto {
   @IsString() readonly credentials?: string;
   @IsString() readonly email?: string;
   @IsEnum(Role) readonly role?: Role;
-  @IsString() readonly dob?: string;
-  @IsEnum(Sex) readonly sex?: Sex;
+  @IsString() @IsOptional() readonly dob?: string;
+  @IsEnum(Sex) @IsOptional() readonly sex?: Sex;
 }
